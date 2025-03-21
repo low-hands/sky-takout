ec6f52f15d184823990bfefb0f5ae4c5f62c7817
Rui Guo <GUOR0401@outlook.com>
2025-03-21 14:24:42 -0700

    微信支付功能（直接跳转到支付成功）

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.User;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -12,5 +14,14 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
+    @AutoFill(OperationType.INSERT)
     void insert(User user);
+
+    /**
+     * 根据id获取用户信息
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 }
