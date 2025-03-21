821a782e7fd2f1de9e30d5b25941b194a76ab0f8
Rui Guo <GUOR0401@outlook.com>
2025-03-21 16:48:12 -0700

    完成订单

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -114,6 +114,17 @@ public class OrderController {
         return Result.success();
     }
 
+    /**
+     * 完成订单
+     *
+     * @return
+     */
+    @PutMapping("/complete/{id}")
+    @ApiOperation("完成订单")
+    public Result complete(@PathVariable("id") Long id) {
+        orderService.complete(id);
+        return Result.success();
+    }
 
 }
 
